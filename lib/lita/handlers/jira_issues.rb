487e98209c3719e2cb017247f0f02729d3a18192
Arthur Maltson <arthur@maltson.com>
2014-10-06 21:16:40 -0400

    Route to detect JIRA issues

--- lib/lita/handlers/jira_issues.rb
+++ lib/lita/handlers/jira_issues.rb
@@ -1,6 +1,9 @@
 module Lita
   module Handlers
     class JiraIssues < Handler
+      route /[A-Z]+-\d+/, :jira_key, help: {
+        "KEY-123" => "Replies with information about the given JIRA key"
+      }
     end
 
     Lita.register_handler(JiraIssues)
--- spec/lita/handlers/jira_issues_spec.rb
+++ spec/lita/handlers/jira_issues_spec.rb
@@ -1,4 +1,6 @@
 require "spec_helper"
 
 describe Lita::Handlers::JiraIssues, lita_handler: true do
+  it { routes('JIRA-123').to(:jira_key) }
+  it { routes('user talking about something JIRA-123 had key').to(:jira_key) }
 end
