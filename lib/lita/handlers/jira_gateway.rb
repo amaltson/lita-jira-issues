cf9c012989385ce14de4d936ade59babd13e8f86
Arthur Maltson <arthur@maltson.com>
2014-10-21 23:35:51 -0400

    Add gateway object for interacting with JIRA API

---
+++ lib/lita/handlers/jira_gateway.rb
@@ -0,0 +1,16 @@
+class JiraGateway
+
+  attr_reader :http, :config
+
+  def initialize(http, config)
+    @http = http
+    @config = config
+  end
+
+  def data_for_issue(key)
+    http.basic_auth(config.username, config.password)
+    response = http.get(config.url + '/rest/api/2/issue/' + key)
+    MultiJson.load(response, symbolize_keys: true)
+  end
+
+end
