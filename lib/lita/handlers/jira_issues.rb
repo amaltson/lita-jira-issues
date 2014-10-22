ef63cda54acebc7f0384f98e21248530104e4fa4
Arthur Maltson <arthur@maltson.com>
2014-10-21 22:54:01 -0400

    Enable configuration for jira_issues

--- lib/lita/handlers/jira_issues.rb
+++ lib/lita/handlers/jira_issues.rb
@@ -1,6 +1,11 @@
 module Lita
   module Handlers
     class JiraIssues < Handler
+
+      def self.default_config(config)
+        config.enabled = true
+      end
+
       route /[A-Z]+-\d+/, :jira_key, help: {
         "KEY-123" => "Replies with information about the given JIRA key"
       }
